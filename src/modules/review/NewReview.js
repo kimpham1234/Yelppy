92fde702105def4ce4d834a2563979daf5a217bf
Peter <qnguyen523@gmail.com>
2017-03-28 19:24:09 -0700

    refresh

--- src/modules/review/NewReview.js
+++
@@ -1,8 +0,0 @@
-import React,{ Component } from 'react';
-import * as firebase from 'firebase';
-import {hashHistory} from 'react-router'
-class NewReview extends Component{
-	render(){
-		// return
-	}
-}
