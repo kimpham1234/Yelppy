f93aba3f5dc1bdbceaea2f590b8db67b91a8ab80
Peter <qnguyen523@gmail.com>
2017-04-05 16:30:39 -0700

    finish homepage

---
+++ src/config/routes.js
@@ -0,0 +1,36 @@
+// routing
+import React from 'react';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router'
+import '../index.css';
+import Restaurants from '../modules/restaurant/Restaurants.js';
+//added by Kim
+import NewRestaurant from '../modules/restaurant/NewRestaurant.js';
+import NewUser from '../modules/user/NewUser.js';
+import Reviews from '../modules/review/Reviews.js';
+import NewReview from '../modules/review/NewReview';
+import Login from '../modules/user/Login.js';
+import Edit from '../modules/review/Edit.js';
+//end by Kim
+
+import Layout from '../modules/home/layout.js';
+import Homepage from '../modules/home/homepage.js';
+import RestaurantDetail from '../modules/restaurant/RestaurantDetail.js';
+import * as requireAuth from './requireAuth.js';
+
+let routes = (
+    <Router history={hashHistory}>
+        <Route path="/" component={Layout}>
+            <Route path='/homepage' component={Homepage}/>
+            <Route path='/restaurants' component={Restaurants}/>
+            <Route path='/restaurants/new' component={NewRestaurant}/>
+            <Route path="/restaurants/:name" component={RestaurantDetail}/>
+            <Route path='/newUser' component={NewUser}/>
+            <Route path='/reviews/edit/:id' component={Edit}/>
+            <Route path='/login' component={Login}/>
+            <Route path='/reviews' component={Reviews}/>
+            <Route path='/reviews/new/:id' component={NewReview}/>
+        </Route>
+    </Router>
+);
+
+module.exports = routes;
