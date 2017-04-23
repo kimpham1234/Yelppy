cda2c76e424ea6b2aa81a52e642302aaf0a1b1a4
fazhaa <cracknmyass@yahoo.com>
2017-04-23 11:58:13 -0700

    branch change

--- src/modules/restaurant/Restaurants.js
+++ src/modules/restaurant/Restaurants.js
@@ -42,39 +42,39 @@ class Restaurants extends Component{
     render(){
         var counter = 1;
         return(
-			<div>
-				<h1>List of restaurants</h1>
-				<Link to='restaurants/new'>New</Link>
-				<Table striped condensed hover responsive>
-					<thead>
-					<tr>
-						<th></th>
-						<th>Name</th>
-						<th>Rating</th>
-						<th>Address</th>
-					</tr>
-					</thead>
-					<tbody>
+            <div>
+                <h1>List of restaurants</h1>
+                <Link to='restaurants/new'>New</Link>
+                <Table striped condensed hover responsive>
+                    <thead>
+                    <tr>
+                        <th></th>
+                        <th>Name</th>
+                        <th>Rating</th>
+                        <th>Address</th>
+                    </tr>
+                    </thead>
+                    <tbody>
                     {this.state.restaurants.map((restaurant, index) => (
-						<tr key={index}>
-							<td>
-								<img src={restaurant.avatar} alt={'Avatar for '+restaurant.name} height="32"></img>
-							</td>
-							<td>
-								<Link to={'/restaurants/'+restaurant.id}>{restaurant.name}</Link>
-							</td>
-							<td>
+                        <tr key={index}>
+                            <td>
+                                <img src={restaurant.avatar} alt={'Avatar for '+restaurant.name} height="32"></img>
+                            </td>
+                            <td>
+                                <Link to={'/restaurants/'+restaurant.id}>{restaurant.name}</Link>
+                            </td>
+                            <td>
                                 { (restaurant.rating == "") ? 0 : restaurant.rating }/5
-							</td>
-							<td>
+                            </td>
+                            <td>
                                 { restaurant.location.display_address[0] + ', '+restaurant.location.display_address[1] }
-							</td>
-						</tr>
+                            </td>
+                        </tr>
 
                     ))}
-					</tbody>
-				</Table>
-			</div>
+                    </tbody>
+                </Table>
+            </div>
         )}
 }
 
