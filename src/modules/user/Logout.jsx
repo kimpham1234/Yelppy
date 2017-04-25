48edbee337c2fde36e823b8e54b499c3abbafc26
cheriemeiyee <cheriemeiyee@gmail.com>
2017-04-25 13:46:56 -0700

    Functional log out button

--- src/modules/user/Logout.jsx
+++ src/modules/user/Logout.jsx
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
 
 class Profile extends Component {
+
+	logOut() {
+    firebase.auth().signOut().then(function() {
+      this.setState({user: null});
+    }.bind(this));
+	}
+
 	render() {
 		return (
 			<div>
-				Logout
+				 <button onClick={this.logOut.bind(this)}>Log Out</button>
 			</div>
 		)
 	}
