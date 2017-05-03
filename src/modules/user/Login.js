df84698b19137caac32695533814e0da4c4e6391
cheriemeiyee <cheriemeiyee@gmail.com>
2017-05-03 10:51:33 -0700

    fix FB new user

--- src/modules/user/Login.js
+++ src/modules/user/Login.js
@@ -34,6 +34,30 @@ class Login extends Component{
 		 	var token = result.credential.accessToken;
 		 // The signed-in user info.
 		 	var user = result.user;
+			var userRef = firebase.database().ref('users');
+			var that = this;
+
+			//check if this is first sign in, if yes save new user to db
+			userRef.orderByChild('email').equalTo(user.email).once('value', function(snapshot){
+				var exist = (snapshot.val() !== null);
+
+				if(!exist){
+					var newUser = userRef.push();
+					var time = Date();
+					var firstAndLast = firebase.auth().currentUser.displayName.split(" ");
+
+					newUser.set({
+						timestamp: time,
+						email: user.email,
+						first: firstAndLast[0],
+						last: firstAndLast[firstAndLast.length-1], //add new user attributes here - Kim
+						numReviews: '0',
+						pictures: [''],
+						reviewed: [''],
+						UID: newUser.key
+					});
+				}
+			});
 		});
 
 		hashHistory.push('/');
@@ -48,10 +72,10 @@ class Login extends Component{
 						<FormControl label="E-mail address" type="text" ref="email" placeholder="Your email address" />
 						<FormControl label="Password" type="password" ref="password" />
 						<FormControl className="btn btn-primary" type="submit" />
-						Or Log In with 
-						<button type="submit">Facebook Login</button>
+						
 					</FormGroup>
 				</form>
+				<button class="loginBtn loginBtn--facebook">Login with Facebook</button>
 			</div>
 			
 		);
