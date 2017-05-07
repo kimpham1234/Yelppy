d98ede5ba21e1ba5ae18822eaa6235f9c331771e
Peter <qnguyen523@gmail.com>
2017-05-07 03:20:06 -0700

    finish resetPassword

--- src/modules/user/EditProfile.js
+++ src/modules/user/EditProfile.js
@@ -2,24 +2,16 @@ import React,{ Component } from 'react';
 import * as firebase from 'firebase';
 import {hashHistory} from 'react-router'
 import "../../App.css";
-
-
 class EditProfile extends Component{
 	constructor(){
 		super();
 	}
-
 	render(){
 		return(
 			<div>
 				Edit profile
 			</div>
-
-
 		)
 	}
-
-
 }
-
 export default EditProfile;
